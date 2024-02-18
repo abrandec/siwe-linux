11f719f51419bba57505220d4b98d90a964facfe
abran <82933273+abrandec@users.noreply.github.com>
2024-02-17 18:10:20 -0800

    most minimal working pam client in Rust

--- crates/pam-tests/src/main.rs
+++ crates/pam-tests/src/main.rs
@@ -1,4 +1,10 @@
+use std::any::Any;
+
 use pam_siwe::misc::Client;
-fn main() {
-    let mut client = Client::start("siwe-auth").expect("Failed to start PAM client");
+
+fn main() {  
+    let mut client = Client::start("siwe-auth").expect("Unable to start PAM client");
+    client
+        .conversation_mut()
+        .set_credentials("user_1", "password");
 }
